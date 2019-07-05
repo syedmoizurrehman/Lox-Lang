1fb26d3ef454172aa112f380acce0165a7469d92
Triple A <abdul.aziz.akmal08@gmail.com>
2019-07-06 03:18:12 +0500

    new change

--- Lox-Lang/Program.cs
+++ Lox-Lang/Program.cs
@@ -6,7 +6,7 @@ namespace LoxLang
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Universe!");
+            Console.WriteLine("Hello galaxy!");
         }
     }
 }
