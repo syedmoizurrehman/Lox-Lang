b5343007c5c7fa4b235f1de9cba234aaeaaef35f
Syed Moiz Ur Rehman <syedmoizurrehman@hotmail.com>
2019-07-06 02:07:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lox_Lang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
